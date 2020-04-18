eefceccf899724cbe2ec0a7847eb2c7fbfd955fa
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-18 23:28:20 +0930

    Start app 'products'

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# . means models.py and admin.py are on the same directory level. 
+# this kind of import called 'relative import'.
+from .models import Product
 
 # Register your models here.
+
+admin.site.register(Product)
