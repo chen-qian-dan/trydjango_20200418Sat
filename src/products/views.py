34445a855037a20bcb59b45ab0fd76bb0bd4384b
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-23 12:18:13 +0930

    Delete and confirm.

--- src/products/views.py
+++ src/products/views.py
@@ -1,21 +1,32 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect 
 from .models import Product 
 from .forms import ProductForm, RawProductForm
 
 # Create your views here.
 
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        # confirming to delete
+        return render(request, 'products/product_delete.html', context)
+        obj.delete()
+     
+    context = {
+        'object': obj
+    }
+    return redirect('../../')
+    
+
+
 def dynamic_lookup_view(request, id):
-    #obj = Product.objects.get(id)
-    #obj = get_object_or_404(Product, id=id)
-    try:
-        obj = Product.objects.get(id=id)
-    except Product.DoesNotExist:
-        raise Http404
+    obj = get_object_or_404(Product, id=id)
+    obj.delete()
     context = {
         'object': obj
     }
     return render(request, 'products/product_detail.html', context)
+
 
 def render_initial_data(request):
     initial_data = {
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -20,7 +20,8 @@ from pages.views import home_view, contact_view, about_view
 from products.views import (
     product_detail_view, 
     product_create_view,  
-    dynamic_lookup_view
+    dynamic_lookup_view,
+    product_delete_view,
 )
 
 urlpatterns = [
@@ -30,5 +31,6 @@ urlpatterns = [
     path('create/', product_create_view),
     path('product/', product_detail_view),
     path('products/<int:id>/', dynamic_lookup_view, name='product'),
+    path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
     path('admin/', admin.site.urls),
 ]
