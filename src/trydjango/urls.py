f56d57d485d6836c791b44c703949736a57d0fa0
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-24 22:47:31 +0930

    Class based views - ListView.

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -20,6 +20,7 @@ from pages.views import home_view, contact_view, about_view
 
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('products/', include('products.urls')),
     path('', home_view, name='home'),
     path('contact/', contact_view),
