abfb851fd1e8cd8678180b2a8c2a336796aca8b5
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-25 11:12:54 +0930

    Class based views - UpdateView.

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import (
     ArticleListView,
     ArticleDetailView,
-    ArticleCreateView
+    ArticleCreateView,
+    ArticleUpdateView
 )
 
 app_name = 'articles'
 urlpatterns = [
      path('', ArticleListView.as_view(), name='article-list'),
-     path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),
+     path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+     path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
      path('create/', ArticleCreateView.as_view(), name='article-create'),
 ]
