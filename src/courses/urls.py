adf77f9c1f0947658272a7fbc3f28874e7dfbb0c
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-27 21:16:41 +0930

    raw create class based view

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     CourseView, 
     CourseListView,
+    CourseCreateView,
     #my_fbv
 )
 
@@ -9,6 +10,7 @@ app_name = 'courses'
 urlpatterns = [
     path('', CourseListView.as_view(), name='courses-list'),
    # path('', CourseView.as_view(template_name='home'), name='courses-list'), 
+    path('create/', CourseCreateView.as_view(), name='courses-create'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
     #path('', my_fbv, name='courses-list'), 
 
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, get_object_or_404 
 from django.views import View 
 
+from .forms import CourseModelForm
 from .models import Course 
 # Create your views here.
 
 # BASE VIEW Class = VIEW 
+
+class CourseCreateView(View):
+    template_name = 'courses/course_create.html'
+    def get(self, request, *args, **kwargs):
+        form = CourseModelForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+
 class CourseListView(View):
     template_name = 'courses/course_list.html'
     queryset = Course.objects.all()
