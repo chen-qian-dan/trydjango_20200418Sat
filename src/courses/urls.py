2fae0cfee12b73806c2b3de9f473190cfeed6b51
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-27 12:30:43 +0930

    Function based view to class based view.

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path 
 from .views import (
-    # CourseView, 
-    my_fbv
+    CourseView, 
+    #my_fbv
 )
 
 app_name = 'courses'
 urlpatterns = [
-    #path('', CourseView.as_view(), name='courses-list'), 
-    path('', my_fbv, name='courses-list'), 
+    path('', CourseView.as_view(), name='courses-list'), 
+    #path('', my_fbv, name='courses-list'), 
 
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views import View 
 
 # Create your views here.
+
+# BASE VIEW Class = VIEW 
+class CourseView(View):
+    def get(self, request, *args, **kwargs):
+        template_name = 'about.html'
+        return render(request, template_name, {})
+
+    # def post(request, *args, **kwargs):
+    #     return render(request, 'about.html', {})  
+
 
 # HTTP METHODS
 def my_fbv(request, *args, **kwargs):
