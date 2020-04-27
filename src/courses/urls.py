f3c95b381ddf0e6fa6125e9b9947183cff8c28a4
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-27 19:51:36 +0930

    raw list class based view

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path 
 from .views import (
     CourseView, 
+    CourseListView,
     #my_fbv
 )
 
 app_name = 'courses'
 urlpatterns = [
-    path('', CourseView.as_view(template_name='home'), name='courses-list'), 
+    path('', CourseListView.as_view(), name='courses-list'),
+   # path('', CourseView.as_view(template_name='home'), name='courses-list'), 
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
     #path('', my_fbv, name='courses-list'), 
 
--- src/courses/views.py
+++ src/courses/views.py
@@ -5,6 +5,18 @@ from .models import Course
 # Create your views here.
 
 # BASE VIEW Class = VIEW 
+class CourseListView(View):
+    template_name = 'courses/course_list.html'
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context = {'object_list': self.get_queryset()}
+        return render(request, self.template_name, context)
+
+
 class CourseView(View): # make CourseView acts for both about and detail view
     template_name = 'courses/course_detail.html'
     def get(self, request, id=None, *args, **kwargs):
